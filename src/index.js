cc86a8c7f15b66336b878d782e73914795a77cbc
Cwiekala Michal <michal.cwiekala-extern@dbschenker.com>
2022-01-23 11:14:03 +0100

    basic parcel

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import Header from "./components/Header";
+import './scss/app.scss'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class HelloMessage extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <div className="container">
+          <h1>Hello {this.props.name}</h1>
+        </div>
+      </div>
+    );
+  }
+}
+
+let App = document.getElementById("app");
+
+ReactDOM.render(<HelloMessage name="Yomi" />, App);
