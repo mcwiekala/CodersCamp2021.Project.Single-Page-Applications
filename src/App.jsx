03888158950a045c470dfad105314d4fe1b61ac4
Judyta Baca <bacajudyta@gmail.com>
2022-02-02 11:18:24 +0100

    initial styles for movie-details

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import "./App.scss";
 import Navbar from "./components/Navbar/Navbar";
 import MoviesView from "./components/movie-viewer/MoviesView";
 
-
 export default function App() {
   return (
     <div className="App">
--- src/components/movie-viewer/MovieDetails.jsx
+++ src/components/movie-viewer/MovieDetails.jsx
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 // import moviesData from "./movies_data.json";
+import "./style/MovieDetails.scss";
 
 export default function MovieDetails() {
   const [movie, setMovie] = useState([]);
+
   const { id } = useParams();
   const apiKey = "46e8c41c";
   const apiUrl = `http://www.omdbapi.com/?apikey=${apiKey}&i=${id}`;
@@ -16,15 +18,46 @@ export default function MovieDetails() {
 
   useEffect(() => {
     getData();
-    console.log(movie);
   }, []);
 
   useEffect(() => null);
 
   return (
-    <>
-      <h1>{movie.Title}</h1>
-      <img src={movie.Poster} alt="" />
-    </>
+    <div className="movie-details-wrapper">
+      <div className="movie-details">
+        <div className="movie-details__poster">
+          <img src={movie.Poster} alt="" />
+        </div>
+        <div className="movie-content-wrapper">
+          <div className="movie-content__title">
+            <h1>{movie.Title}</h1>
+            <h4>
+              {movie.Genre} | {movie.Rated} | {movie.Runtime}
+            </h4>
+            <p>{movie.Plot}</p>
+          </div>
+          <div className="movie-facts-wrapper">
+            <div className="movie-facts__column">
+              <h4> Release Date: </h4>
+              <h4 className="movie-facts--description">{movie.Released}</h4>
+              <h4> Production:</h4>
+              <h4 className="movie-facts--description">
+                {movie.Country}, {movie.Year}
+              </h4>
+              <h4> Cast: </h4>
+              <h4 className="movie-facts--description">{movie.Actors}</h4>
+              <h4> Awards: </h4>
+              <h4 className="movie-facts--description">{movie.Awards}</h4>
+            </div>
+            <div className="movie-facts__rating">
+              <h4>Average rating:</h4>
+              <div className="movie-facts__rating--score">
+                {movie.imdbRating}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
