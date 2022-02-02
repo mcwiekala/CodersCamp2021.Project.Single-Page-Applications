6a61f34b7f4b43f34f32940a16991904111d4812
Judyta Baca <bacajudyta@gmail.com>
2022-02-02 09:41:55 +0100

    fetch a movie form omdb

--- src/components/movie-viewer/MovieDetails.jsx
+++ src/components/movie-viewer/MovieDetails.jsx
@@ -1,20 +1,30 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import moviesData from "./movies_data.json";
+// import moviesData from "./movies_data.json";
 
 export default function MovieDetails() {
-  const [movie, setMovie] = useState(null);
+  const [movie, setMovie] = useState([]);
   const { id } = useParams();
+  const apiKey = "46e8c41c";
+  const apiUrl = `http://www.omdbapi.com/?apikey=${apiKey}&i=${id}`;
+
+  const getData = () => {
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => setMovie(data));
+  };
 
   useEffect(() => {
-    setMovie(moviesData.movies.filter((item) => item.imdbID === id));
+    getData();
     console.log(movie);
   }, []);
 
+  useEffect(() => null);
+
   return (
     <>
-      <h1>My Movie Overview</h1>
-      <h3>ID: {id}</h3>
+      <h1>{movie.Title}</h1>
+      <img src={movie.Poster} alt="" />
     </>
   );
 }
