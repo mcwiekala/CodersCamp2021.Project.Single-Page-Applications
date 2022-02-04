093780bc54679130a0693e842c7764980a973148
TARGEToff <lockontargt@gmail.com>
2022-02-04 23:52:51 +0100

    some test data

---
+++ src/pages/seat-reservation/data.js
@@ -0,0 +1,96 @@
+const cinemaHalls = [
+        {
+            numberOfSeats: { rows: 9, columns: 14 },
+            filmShowing: [
+                {
+                  id: 1,
+                  title: "Avengers",
+                  seatsTakes: [
+                  { row: 4, column: 5 },
+                  { row: 4, column: 6 },
+                  { row: 2, column: 8 },
+                  { row: 2, column: 5 },
+                  { row: 9, column: 10 },
+                  { row: 4, column: 12 },
+                  { row: 3, column: 1 },
+                    ],
+                  ticketPrice: [15.99, 29.99]
+                },
+                {
+                    id: 2,
+                    title: "Iniemamocni",
+                    seatsTakes: [
+                    { row: 4, column: 5 },
+                    { row: 4, column: 6 },
+                    { row: 2, column: 8 },
+                    { row: 2, column: 5 },
+                    { row: 9, column: 10 },
+                    { row: 4, column: 12 },
+                    { row: 3, column: 1 },
+                      ],
+                    ticketPrice: [15.99, 29.99]
+                  },
+                  {
+                    id: 3,
+                    title: "Skazani na Shawshank",
+                    seatsTakes: [
+                    { row: 4, column: 5 },
+                    { row: 4, column: 6 },
+                    { row: 2, column: 8 },
+                    { row: 2, column: 5 },
+                    { row: 9, column: 10 },
+                    { row: 4, column: 12 },
+                    { row: 3, column: 1 },
+                      ],
+                    ticketPrice: [15.99, 29.99]
+                  },
+              ]             
+        },
+        {
+            numberOfSeats: { rows: 10, columns: 16 },
+            filmShowing: [
+                {
+                  id: 1,
+                  title: "Avengers",
+                  seatsTakes: [
+                  { row: 4, column: 5 },
+                  { row: 4, column: 6 },
+                  { row: 2, column: 8 },
+                  { row: 2, column: 5 },
+                  { row: 9, column: 10 },
+                  { row: 4, column: 12 },
+                  { row: 3, column: 1 },
+                    ],
+                  ticketPrice: [15.99, 29.99]
+                },
+                {
+                    id: 2,
+                    title: "Iniemamocni",
+                    seatsTakes: [
+                    { row: 4, column: 5 },
+                    { row: 4, column: 6 },
+                    { row: 2, column: 8 },
+                    { row: 2, column: 5 },
+                    { row: 9, column: 10 },
+                    { row: 4, column: 12 },
+                    { row: 3, column: 1 },
+                      ],
+                    ticketPrice: [15.99, 29.99]
+                  },
+                  {
+                    id: 3,
+                    title: "Skazani na Shawshank",
+                    seatsTakes: [
+                    { row: 4, column: 5 },
+                    { row: 4, column: 6 },
+                    { row: 2, column: 8 },
+                    { row: 2, column: 5 },
+                    { row: 9, column: 10 },
+                    { row: 4, column: 12 },
+                    { row: 3, column: 1 },
+                      ],
+                    ticketPrice: [15.99, 29.99]
+                  },
+              ]             
+        },
+    ]
